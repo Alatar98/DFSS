ea254ced50d2337dd70d72eaa34a70c80ce7cfd4
Tobias <frey.tobi98@gmail.com>
2023-12-06 17:17:59 +0100

    added excluding scaler and stder

--- TSST.py
+++ TSST.py
@@ -11,6 +11,9 @@ import numpy as np
 import pandas as pd
 from  statsmodels.formula.api import ols
 
+from sklearn.preprocessing import StandardScaler
+
+
 
 def M_regression(X, Y, M=1):
     Ms = list(range(1,M+1))
@@ -28,3 +31,63 @@ def M_regression(X, Y, M=1):
         Ms.remove(fit.pvalues.argmax())
 
 
+def Mult_M_regression():
+    return "not done"
+
+class ExcludingScaler(StandardScaler):
+    def __init__(self, exclude_col=None, **kwargs):
+        self.exclude_col = exclude_col
+        super().__init__(**kwargs)
+
+    def fit(self, X):
+        if self.exclude_col is not None and self.exclude_col in X.columns:
+            X_to_fit = X.drop(self.exclude_col, axis=1)
+            super().fit(X_to_fit)
+        else:
+            super().fit(X_to_fit)
+        return self
+
+    def std(self, X):
+        if self.exclude_col is not None and self.exclude_col in X.columns:
+            # Exclude the specified column
+            X_to_scale = X.drop(self.exclude_col, axis=1)
+
+            # Perform operations on the DataFrame without the excluded column
+            # For example, you can apply standardization using the scaler
+            X_transformed = pd.DataFrame(self.transform(X_to_scale), index=X_to_scale.index, columns=X_to_scale.columns)
+
+
+            # Add the column back to the DataFrame
+            X_transformed.insert(0, self.exclude_col, X[self.exclude_col])
+
+        else:
+            # No column to exclude, simply apply standardization
+            X_to_scale = X
+            X_transformed = pd.DataFrame(self.transform(X_to_scale), index=X_to_scale.index, columns=X_to_scale.columns)
+
+        return X_transformed
+    
+    def rev(self, X):
+        if self.exclude_col is not None and self.exclude_col in X.columns:
+            # Exclude the specified column
+            X_to_scale = X.drop(self.exclude_col, axis=1)
+            # Perform operations on the DataFrame without the excluded column
+            # For example, you can apply standardization using the scaler
+            X_transformed = pd.DataFrame(self.inverse_transform(X_to_scale), index=X_to_scale.index, columns=X_to_scale.columns)
+
+            # Add the column back to the DataFrame
+            X_transformed.insert(0, self.exclude_col, X[self.exclude_col])
+        else:
+            # No column to exclude, simply apply standardization
+            X_to_scale = X
+            X_transformed = pd.DataFrame(self.inverse_transform(X_to_scale), index=X_to_scale.index, columns=X_to_scale.columns)
+        
+        return X_transformed
+
+
+
+def stder(X, exclude_col=None):
+    scaler = ExcludingScaler(exclude_col)
+    fitted = scaler.fit(X)
+    x_std = fitted.std(X)
+    return x_std, fitted
--- TestingTheTools.py
+++ TestingTheTools.py
@@ -19,3 +19,39 @@ result = TT.M_regression(df["tmp"],df["volt"],3)
 
 print(result.summary())
 
+
+# Sample DataFrame
+data = {'A': [1, 2, 3, 4, 5],
+        'B': [10, 20, 30, 40, 50],
+        'C': [100, 200, 300, 400, 500]}
+df = pd.DataFrame(data)
+
+# Row to exclude for standardization
+col_to_exclude = 'A'
+
+# Standardize the DataFrame excluding one row
+data_to_standardize = df.drop(columns=col_to_exclude)
+df_scal, scaler = TT.stder(df,'A')
+
+# Additional data to standardize
+new_data = pd.DataFrame({'A': [6], 'B': [60], 'C': [600]})
+
+# Apply the same standardization to additional data
+standardized_new_data = scaler.std(new_data)
+
+
+# Additional data to reverse
+new_data = pd.DataFrame({'A': [7], 'B': [2.5], 'C': [2.5]})
+
+# Apply the same standardization to additional data
+reversed_new_data = scaler.rev(new_data)
+
+# Display results
+print("Original DataFrame:")
+print(df)
+print("\nStandardized DataFrame:")
+print(df_scal)
+print("\nStandardized Additional Data:")
+print(standardized_new_data)
+print("\nReverse Additional Data:")
+print(reversed_new_data)
