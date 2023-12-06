6c35cf8f928d332f44e75d0ee0998e7bfb44f97e
Tobias <frey.tobi98@gmail.com>
2023-12-06 15:49:26 +0100

    Added TSST.py

---
+++ TSST.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+TobisSixSigTools
+
+Created on Wed Dec 06 15:48:43 2023
+
+@author: freyt
+"""
+
+import numpy as np
+import pandas as pd
+from  statsmodels.formula.api import ols
